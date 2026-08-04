786f9ddc3dc595d2fe8506c3788b503d089ef94b
Kai A <kai.andresen@pm.me>
2026-08-04 19:26:30 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./content/**/*.md",
+    "./layouts/**/*.html",
+    "./assets/**/*.{js,jsx,ts,tsx}",
+    "./themes/**/layouts/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
